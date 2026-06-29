813b8204927ae46af37f60c27d567502f9d61039
daviaraujojwm <davi.araujo@jwmlogistica.com.br>
2026-06-29 13:29:08 -0300

    Update 3d.py

--- 3d.py
+++ 3d.py
@@ -899,7 +899,7 @@ if "cargas" not in st.session_state:
 st.markdown(f"""
 <div class="header-jwm">
     <div class="titulo-jwm">
-        
+        Dimensionamento 3D
     </div>
 </div>
 """, unsafe_allow_html=True)
